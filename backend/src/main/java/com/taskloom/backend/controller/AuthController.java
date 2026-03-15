58c91da999d9a7350449ef606f1ced47c5e422b4
Sukanta Dutta <sukantadutta2003@gmail.com>
2026-03-16 00:57:26 +0530

    Saved changes

--- backend/src/main/java/com/taskloom/backend/controller/AuthController.java
+++ backend/src/main/java/com/taskloom/backend/controller/AuthController.java
@@ -6,7 +6,7 @@ import com.taskloom.backend.dto.AuthRequest;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/auth")
